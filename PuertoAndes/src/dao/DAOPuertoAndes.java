4098f98bb87f08d1d2356e1ac36dbe21a78f46f0
camendoza94 <ca.mendoza968@uniandes.edu.co>
2016-03-13 10:34:35 -0500

    DAO y Transacciones 0.1

--- PuertoAndes/src/dao/DAOPuertoAndes.java
+++ PuertoAndes/src/dao/DAOPuertoAndes.java
@@ -1,5 +1,225 @@
+/**-------------------------------------------------------------------
+ * $Id$
+ * 
+ * Universidad de los Andes (Bogot� - Colombia)
+ * Departamento de Ingenier�a de Sistemas y Computaci�n
+ *
+ * Materia: Sistemas Transaccionales
+ * Extractos tomados de VideoAndes por Juan Felipe Garc�a - jf.garcia268@uniandes.edu.co
+ * -------------------------------------------------------------------
+ */
 package dao;
 
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import vos.*;
+
+/**
+ * Clase DAO que se conecta la base de datos usando JDBC para resolver los requerimientos de la aplicación
+ * @author Juan
+ */
 public class DAOPuertoAndes {
+
+
+	/**
+	 * Arraylits de recursos que se usan para la ejecución de sentencias SQL
+	 */
+	private ArrayList<Object> recursos;
+
+	/**
+	 * Atributo que genera la conexión a la base de datos
+	 */
+	private Connection conn;
+
+	/**
+	 * Método constructor que crea DAOVideo
+	 * <b>post: </b> Crea la instancia del DAO e inicializa el Arraylist de recursos
+	 */
+	public DAOPuertoAndes() {
+		recursos = new ArrayList<Object>();
+	}
+
+	/**
+	 * Método que cierra todos los recursos que estan enel arreglo de recursos
+	 * <b>post: </b> Todos los recurso del arreglo de recursos han sido cerrados
+	 */
+	public void cerrarRecursos() {
+		for(Object ob : recursos){
+			if(ob instanceof PreparedStatement)
+				try {
+					((PreparedStatement) ob).close();
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				}
+		}
+	}
+
+	/**
+	 * Método que inicializa la connection del DAO a la base de datos con la conexión que entra como parámetro.
+	 * @param con  - connection a la base de datos
+	 */
+	public void setConn(Connection con){
+		this.conn = con;
+	}
+
+
+	/**
+	 * Método que, usando la conexión a la base de datos, saca todos los videos de la base de datos
+	 * <b>SQL Statement:</b> SELECT * FROM VIDEOS;
+	 * @return Arraylist con los videos de la base de datos.
+	 * @throws SQLException - Cualquier error que la base de datos arroje.
+	 * @throws Exception - Cualquier error que no corresponda a la base de datos
+	 */
+	public ArrayList<Video> darVideos() throws SQLException, Exception {
+		ArrayList<Video> videos = new ArrayList<Video>();
+
+		String sql = "SELECT * FROM VIDEOS";
+
+		PreparedStatement prepStmt = conn.prepareStatement(sql);
+		recursos.add(prepStmt);
+		ResultSet rs = prepStmt.executeQuery();
+
+		while (rs.next()) {
+			String name = rs.getString("NAME");
+			int id = Integer.parseInt(rs.getString("ID"));
+			int duration = Integer.parseInt(rs.getString("DURATION"));
+			videos.add(new Video(id, name, duration));
+		}
+		return videos;
+	}
+
+
+	/**
+	 * Método que busca el/los videos con el nombre que entra como parámetro.
+	 * @param name - Nombre de el/los videos a buscar
+	 * @return Arraylist con los videos encontrados
+	 * @throws SQLException - Cualquier error que la base de datos arroje.
+	 * @throws Exception - Cualquier error que no corresponda a la base de datos
+	 */
+	public ArrayList<Video> buscarVideosPorName(String name) throws SQLException, Exception {
+		ArrayList<Video> videos = new ArrayList<Video>();
+
+		String sql = "SELECT * FROM VIDEOS WHERE NAME ='" + name + "'";
+
+		System.out.println("SQL stmt:" + sql);
+
+		PreparedStatement prepStmt = conn.prepareStatement(sql);
+		recursos.add(prepStmt);
+		ResultSet rs = prepStmt.executeQuery();
+
+		while (rs.next()) {
+			String name2 = rs.getString("NAME");
+			int id = Integer.parseInt(rs.getString("ID"));
+			int duration = Integer.parseInt(rs.getString("DURATION"));
+			videos.add(new Video(id, name2, duration));
+		}
+
+		return videos;
+	}
+
+	/**
+	 * Método que agrega el video que entra como parámetro a la base de datos.
+	 * @param video - el video a agregar. video !=  null
+	 * <b> post: </b> se ha agregado el video a la base de datos en la transaction actual. pendiente que el video master
+	 * haga commit para que el video baje  a la base de datos.
+	 * @throws SQLException - Cualquier error que la base de datos arroje. No pudo agregar el video a la base de datos
+	 * @throws Exception - Cualquier error que no corresponda a la base de datos
+	 */
+	public void addVideo(Video video) throws SQLException, Exception {
+
+		String sql = "INSERT INTO VIDEOS VALUES (";
+		sql += video.getId() + ",'";
+		sql += video.getName() + "',";
+		sql += video.getDuration() + ")";
+
+		System.out.println("SQL stmt:" + sql);
+
+		PreparedStatement prepStmt = conn.prepareStatement(sql);
+		recursos.add(prepStmt);
+		prepStmt.executeQuery();
+
+	}
+	
+	/**
+	 * Método que actualiza el video que entra como parámetro en la base de datos.
+	 * @param video - el video a actualizar. video !=  null
+	 * <b> post: </b> se ha actualizado el video en la base de datos en la transaction actual. pendiente que el video master
+	 * haga commit para que los cambios bajen a la base de datos.
+	 * @throws SQLException - Cualquier error que la base de datos arroje. No pudo actualizar el video.
+	 * @throws Exception - Cualquier error que no corresponda a la base de datos
+	 */
+	public void updateVideo(Video video) throws SQLException, Exception {
+
+		String sql = "UPDATE VIDEOS SET ";
+		sql += "name='" + video.getName() + "',";
+		sql += "duration=" + video.getDuration();
+		sql += " WHERE id = " + video.getId();
+
+		System.out.println("SQL stmt:" + sql);
+
+		PreparedStatement prepStmt = conn.prepareStatement(sql);
+		recursos.add(prepStmt);
+		prepStmt.executeQuery();
+	}
+
+	/**
+	 * Método que elimina el video que entra como parámetro en la base de datos.
+	 * @param video - el video a borrar. video !=  null
+	 * <b> post: </b> se ha borrado el video en la base de datos en la transaction actual. pendiente que el video master
+	 * haga commit para que los cambios bajen a la base de datos.
+	 * @throws SQLException - Cualquier error que la base de datos arroje. No pudo actualizar el video.
+	 * @throws Exception - Cualquier error que no corresponda a la base de datos
+	 */
+	public void deleteVideo(Video video) throws SQLException, Exception {
+
+		String sql = "DELETE FROM VIDEOS";
+		sql += " WHERE id = " + video.getId();
+
+		System.out.println("SQL stmt:" + sql);
+
+		PreparedStatement prepStmt = conn.prepareStatement(sql);
+		recursos.add(prepStmt);
+		prepStmt.executeQuery();
+	}
+
+	/**
+	 * Método que busca el/los videos mas alquilados.
+	 * @return Arraylist con los videos encontrados
+	 * @throws SQLException - Cualquier error que la base de datos arroje.
+	 * @throws Exception - Cualquier error que no corresponda a la base de datos
+	 */
+	public ArrayList<Video> darVideoMasAlquilado()  throws SQLException, Exception {
+		ArrayList<Video> videos = new ArrayList<Video>();
+
+		String sql = "SELECT * " +
+					 "FROM VIDEOS " +
+				     "WHERE VIDEOS.ID IN (SELECT VIDEO_ID " +
+				                         "FROM ALQUILERES " +
+				                         "GROUP BY VIDEO_ID " +
+				                         "HAVING COUNT(*) = (SELECT MAX(COUNT(*)) " +
+				                                            "FROM ALQUILERES " +
+				                                            "GROUP BY VIDEO_ID)) ";
+
+		System.out.println("SQL stmt:" + sql);
+
+		PreparedStatement prepStmt = conn.prepareStatement(sql);
+		recursos.add(prepStmt);
+		ResultSet rs = prepStmt.executeQuery();
+
+		while (rs.next()) {
+			String name = rs.getString("NAME");
+			int id = Integer.parseInt(rs.getString("ID"));
+			int duration = Integer.parseInt(rs.getString("DURATION"));
+			videos.add(new Video(id, name, duration));
+		}
+
+		return videos;
+	}
+
 
 }
